27a0fce4eba0564434e2652a08e9cb4111a74959
gabriel.muller <gabriel.muller@globant.com>
2018-09-13 17:52:04 -0300

    Map - loader script - WIP

--- src/components/Map/GoogleApiComponent/GoogleLoader.js
+++ src/components/Map/GoogleApiComponent/GoogleLoader.js
@@ -1,30 +1,43 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import GoogleApiConfig from './GoogleApiConfig';
 
-const createScriptJs = (() => {
+const createScriptJs = (initName) => {
   const _script = document.createElement('script');
-  _script.src = `${GoogleApiConfig.url}?key=${GoogleApiConfig.apiKey}&&callback=initMap`;
+  _script.src = `${GoogleApiConfig.url}?key=${GoogleApiConfig.apiKey}&&callback=${initName}`;
   _script.async = true;
   _script.defer = true;
   return _script;
-})();
+};
 
-const GoogleLoader = ({ coords, zoom, refNode = 'map' }) => {
-  var map = null;
+const GoogleLoaderWithScript = (WrappedComponent, { idMap = 'map', props }) => {
 
-  window.addEventListener('load', function () {
-    document.getElementsByTagName('body')[0].appendChild(createScriptJs);
-  });
+  return class extends Component {
 
-  function initMap() {
-    const options = { center:coords, zoom };
-    map = new window.google.maps.Map(document.getElementById(refNode), options);
-  }
+    constructor(props){
+      super(props);
+      this.state = {
+        map: null,
+        displayInitName: 'initMap'
+      }
+    }
 
-  return function bootstrapMap(GoogleMap) {
-    return <GoogleMap map={map} idMap={refNode} />
+    initMap (){
+      this.setState({
+        map: new window.google.maps.Map(document.getElementById(idMap), { ...props }),
+      });
+    }
+
+    componentDidMount (){
+      document.getElementById('root').addEventListener('load', function () {
+        document.getElementsByTagName('body')[0].appendChild(createScriptJs(this.state.displayInitName));
+      });
+    }
+
+    render (){
+      return <WrappedComponent map={this.state.map} idMap={idMap} />
+    }
   }
 }
 
-export default GoogleLoader;
+export default GoogleLoaderWithScript;
--- src/components/Map/WeatherMap.js
+++ src/components/Map/WeatherMap.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
 
-import GoogleLoader from './GoogleApiComponent/GoogleLoader';
+import GoogleLoaderWithScript from './GoogleApiComponent/GoogleLoader';
 import GoogleMap from './GoogleApiComponent/GoogleMap';
 
 const WeatherMap = () => {
-    return GoogleLoader({
-        coords: {},
-        zoom: 8
-    })(GoogleMap);
-}
+    const GoogleMapWithScript = GoogleLoaderWithScript(GoogleMap, { idMap: 'map', center: {lat: -34.397, lng: 150.644}, zoom: 8 })
+    return <GoogleMapWithScript />;
+};
 
 export default WeatherMap;
